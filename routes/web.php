3e4db9b31dd8aab18214b44910ede191d86906ad
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-10 20:05:48 +0200

    commit 3 - fix controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Guests\PageController as GuestsPageController;
 use App\Http\Controllers\Admin\PageController as AdminPageController;
 
@@ -19,6 +20,17 @@ use App\Http\Controllers\Admin\PageController as AdminPageController;
 Route::get('/', [GuestsPageController::class, 'home'])->name('guest.home');
 
 Route::get('/admin', [AdminPageController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+Route::middleware('auth', 'verified')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [AdminPageController::class, 'dashboard'])->name('dashboard');
+        Route::get('/projects/trashed', [ProjectController::class, 'trashed'])->name('projects.trashed');
+        Route::delete('/projects/{project}/harddelete', [ProjectController::class, 'harddelete'])->name('projects.harddelete');
+        Route::post('/projects/{project}/restore', [ProjectController::class, 'restore'])->name('projects.restore');
+        Route::resource('projects', ProjectController::class);
+    });
 
 Route::middleware('auth')
     ->name('admin.')
