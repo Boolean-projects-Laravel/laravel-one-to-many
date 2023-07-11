94a04407b07c19550c3b17fc3b687e3c088e9add
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-11 13:33:53 +0200

    commit 6 - fix show Type

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,11 +5,13 @@
         <thead>
             <tr>
                 <th scope="col">Title</th>
+                <th scope="col">Id</th>
                 <th scope="col">Author</th>
                 <th scope="col">Creation Date</th>
                 <th scope="col">Last Update</th>
                 <th scope="col">Collaborators</th>
                 <th scope="col">Description</th>
+                <th scope="col">Type</th>
                 <th scope="col">Languages</th>
                 <th scope="col">Link</th>
                 <th scope="col">Actions</th>
@@ -18,13 +20,22 @@
         <tbody>
             <tr>
                 <th scope="row">{{ $project->title }}</th>
+                <th scope="row">{{ $project->id }}</th>
                 <td>{{ $project->author }}</td>
                 <td>{{ $project->creation_date}}</td>
                 <td>{{ $project->last_update }}</td>
                 <td>{{ $project->collaborators }}</td>
                 <td>{{ $project->description }}</td>
+                <td>
+                    <a 
+                    class="text-decoration-none" 
+                    href="{{ route('admin.types.show', ['type' => $project->type]) }}"
+                    >
+                    {{ $project->type->name }}
+                    </a>
+                </td>
                 <td>{{ $project->languages }}</td>
-                <td><a href="{{ $project->link_github }}">Link</a></td>
+                <td><a class="text-decoration-none" href="{{ $project->link_github }}">Link</a></td>
                 
                 <td>
                     <a class="btn btn-warning" href="{{ route('admin.projects.edit', ['project' => $project->id]) }}">Edit</a>
